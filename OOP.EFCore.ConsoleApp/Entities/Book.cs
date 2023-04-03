c89f76ac66a00c08143f1236e740f14a982a52a3
Mustafa SARIGÜL <msarigul60@gmail.com>
2023-04-03 10:42:15 +0300

    propertyconstraint

--- OOP.EFCore.ConsoleApp/Entities/Book.cs
+++ OOP.EFCore.ConsoleApp/Entities/Book.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,8 +9,10 @@ namespace OOP.EFCore.ConsoleApp.Entities
 {
     public class Book
     {
+        [Key]
         public int BookId { get; set; }
-
+        [Required]
+        [MaxLength(50)]
         public string? Title { get; set; }   
     }
 }
